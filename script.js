b44d50749cff8f8fa3ff6c6191ade0a0fabcee11
Alyan Salman <alyansalman11111@gmail.com>
2026-06-23 21:59:03 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Study Skill Tracker Loaded");
+function addSkill()
+{
+    let input = document.getElementById("AddSkill");
+    let skill = input.value;
+
+    if(skill == "")
+        return;
+
+    let list = document.getElementById("skillList");
+
+    let li = document.createElement("li");
+    li.textContent = skill;
+
+    list.appendChild(li);
+
+    input.value = "";
+}
