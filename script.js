8f39ca97a6bbb7293a994d7e1bdcf957981397b5
Alyan Salman <alyansalman11111@gmail.com>
2026-06-24 15:10:49 +0500

    Update script.js

--- script.js
+++ script.js
@@ -6,19 +6,23 @@ function addSkill()
     let skill = input.value.trim();
 
     if(skill == "")
-        return
+        return;
 
     tasks.push({id: Date.now(), title: skill, completed: false});
-    
+
     input.value = "";
-    
+
     renderTasks();
 }
 
 function deleteTask(id)
 {
     tasks = tasks.filter(task => task.id !== id)
+}
 
+function toggleTask(id)
+{
+    tasks.forEach(task => {if(task.id == id){ task.completed = !task.completed;}})
     renderTasks();
 }
 
@@ -30,19 +34,32 @@ function renderTasks()
     tasks.forEach(task => 
     {
         let li = document.createElement("li");
+        let checkbox = document.createElement("input");
+
+        checkbox.type = "checkbox";
+        checkbox.checked = task.completed;
+
+        checkbox.onclick = function () { toggleTask(task.id); };
+
         let taskText = document.createElement("span");
-        
         taskText.textContent = task.title;
 
+        if(task.completed)
+        {
+            taskText.style.textDecoration = "line-through";
+            taskText.style.opacity = "0.5";
+        }
+
         let deleteButton = document.createElement("button");
-
         deleteButton.textContent = "X";
-        deleteButton.classList.add("delete-btn");
+        deleteButton.classList.add("delete-btn")
 
-        deleteButton.onclick = function () { deleteTask(task.id)};
+        deleteButton.onclick = function() {deleteTask(task.id);};
 
+        li.appendChild(checkbox);
         li.appendChild(taskText);
         li.appendChild(deleteButton);
+
         list.appendChild(li);
-    })
+    });
 }
